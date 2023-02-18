aa05da955555f6c4075139fea4b28d73eed7e6ec
Benoit Gaudet <bgaudet38@gmail.com>
2023-02-18 19:15:00 +0000

    Static datas and itinerary classes

--- app/src/main/java/com/example/travelapp/MainActivity.kt
+++ app/src/main/java/com/example/travelapp/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import com.example.travelapp.itineraries.StaticData
 
 class MainActivity : AppCompatActivity() {
+    val itineraryList = StaticData().itineraryList
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
