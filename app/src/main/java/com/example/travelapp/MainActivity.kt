2209bf59ffd02f82aa5169d31c6aa662b562a9be
melodybucchino <melodybucchino@gmail.com>
2023-02-18 18:03:05 +0000

    Added first UI to main activity and created SecondActivity for when 'plus' button is pushed

--- app/src/main/java/com/example/travelapp/MainActivity.kt
+++ app/src/main/java/com/example/travelapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.travelapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            val createButton = findViewById<ImageButton>(R.id.createButton)
+            createButton.setOnClickListener{
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent)
+
+
+
+            }
+
+
+
+
     }
 }
