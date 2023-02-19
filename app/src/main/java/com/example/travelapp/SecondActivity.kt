800f194411c8a115f02dd7fdaab25e769db7d895
ggmcg1825 <mcgeeth@tcd.ie>
2023-02-19 17:16:13 +0000

    Added itineraries now show on main screen

--- app/src/main/java/com/example/travelapp/SecondActivity.kt
+++ app/src/main/java/com/example/travelapp/SecondActivity.kt
@@ -1,11 +1,18 @@
 package com.example.travelapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import com.example.travelapp.itineraries.Itinerary
+//import com.example.travelapp.itineraries.StaticData
+import com.example.travelapp.itineraries.Step
+import com.example.travelapp.MainActivity.Companion.itineraryList
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity(){
+    //val itineraryList = StaticData.itineraryList
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -14,5 +21,22 @@ class SecondActivity : AppCompatActivity() {
                 val intent = Intent(this, CreateItem::class.java)
                 startActivity(intent)
             }
+            val createitsubmit = findViewById<Button>(R.id.itsubmit)
+            createitsubmit.setOnClickListener{
+                val Itin_Title = findViewById<EditText>(R.id.Itin_Title)
+                val newItinerary = Itinerary(Itin_Title.text.toString())
+                // statement to replace blank names, not needed but *aesthetics*
+                if (newItinerary.name == "") newItinerary. name = "Itinerary"
+                newItinerary.steps.add(Step("Trinity College", "College Green, Dublin 2", 0f, "Nice place to visit"))
+                //StaticData().itineraryList.add(newItinerary)
+                itineraryList.add(newItinerary)
+
+
+                // Return to previous page
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
     }
 }
+
