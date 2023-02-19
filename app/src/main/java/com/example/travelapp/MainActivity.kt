0e89e8ddb5953e6d7ddfccf7cd5cc67d2d9b9e9e
Benoit Gaudet <bgaudet38@gmail.com>
2023-02-19 16:54:04 +0000

    Add title in main page

--- app/src/main/java/com/example/travelapp/MainActivity.kt
+++ app/src/main/java/com/example/travelapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.ListView
 import com.example.travelapp.adapters.ItineraryListAdapter
 import com.example.travelapp.itineraries.StaticData
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-            val createButton = findViewById<ImageButton>(R.id.createButton)
+            val createButton = findViewById<ImageView>(R.id.createButton)
             createButton.setOnClickListener{
                 val intent = Intent(this, SecondActivity::class.java)
                 startActivity(intent)
